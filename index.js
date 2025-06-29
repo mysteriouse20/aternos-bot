6049fa52d0d285ea703a0a010ff1c278b695b8e8
mysteriouse20 <siahmedsamy36@gmail.com>
2025-06-30 00:52:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const mineflayer = require("mineflayer");
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: "NqouchaTeam.aternos.me",
+    port: 43107,
+    username: "AFK_Bot",
+    version: "1.21.6"
+  });
+
+  bot.on("spawn", () => {
+    console.log("✅ Logged in!");
+    bot.chat("AFK Bot is now online!");
+  });
+
+  bot.on("end", () => {
+    console.log("❌ Bot disconnected, reconnecting...");
+    setTimeout(createBot, 5000);
+  });
+
+  bot.on("error", (err) => {
+    console.log("Error: ", err);
+  });
+}
+
+createBot();
